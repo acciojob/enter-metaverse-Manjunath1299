0f9e1a125346f798858e088cd76e3c796701de30
Manjunath <66263712+Manjunath1299@users.noreply.github.com>
2023-12-23 19:28:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-function updateContent() {
-    var statusElement = document.getElementById('status');
-    
-    // Change the tag to <h1>
-    var h1Element = document.createElement('h1');
-    h1Element.textContent = "Entered Metaverse";
+ function changeText() {
+            var statusElement = document.getElementById('status');
 
-    // Replace the existing <p> with the new <h1>
-    statusElement.parentNode.replaceChild(h1Element, statusElement);
-}
+            // Change the tag to <h1>
+            var h1Element = document.createElement('h1');
+            h1Element.id = 'status';
+            h1Element.textContent = 'Entered Metaverse';
+
+            // Replace the <p> with the new <h1>
+            statusElement.parentNode.replaceChild(h1Element, statusElement);
+        }
