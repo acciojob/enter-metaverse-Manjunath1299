0d99168244b7fdb30596287eddcdfde483bdec85
Manjunath <66263712+Manjunath1299@users.noreply.github.com>
2023-12-23 19:22:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+function updateContent() {
+    var statusElement = document.getElementById('status');
+    
+    // Change the tag to <h1>
+    var h1Element = document.createElement('h1');
+    h1Element.textContent = "Entered Metaverse";
+
+    // Replace the existing <p> with the new <h1>
+    statusElement.parentNode.replaceChild(h1Element, statusElement);
+}
