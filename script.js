f591ee69aa9b4979b3d1fc563dba6002aa95afd7
Manjunath <66263712+Manjunath1299@users.noreply.github.com>
2023-12-28 19:24:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ function updateContent() {
   // Get the p element with the id "status"
   const statusP = document.getElementById("status");
 
-  // Create an h1 element with the new text
+  
   const newContent = document.createElement("h1");
   newContent.textContent = "Entered Metaverse";
 
-  // Replace the existing content of the p element with the h1 element
-  statusP.innerHTML = ""; // Clear the existing content
-  statusP.appendChild(newContent); // Add the h1 element
+  statusP.innerHTML = ""; 
+  statusP.appendChild(newContent); 
 }
